eff66a4aa914cc2b89cb2e9b8be864165ae819ad
Paul Kim <ppaauullkkiimm@gmail.com>
2025-01-14 17:45:19 +0100

    api-service with axios

---
+++ src/service/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const api = axios.create({ baseURL: "https://fakestoreapi.com" });
+
+export const getProducts = (limit = 20) => api.get(`/products?limit=${limit}`);
+export const getSingleProduct = (productId) =>
+  api.get(`/products/${productId}`);
+export const getCategories = () => api.get("/products/categories");
+export const getProductsByCategory = (category) =>
+  api.get(`/products/category/${category}`);
+export const addCart = (cartData) => api.post("/carts", cartData);
+export const updateCart = (cartId, cartData) =>
+  api.put(`/carts/${cartId}`, cartData);
+export const patchCart = (cartId, cartData) =>
+  api.patch(`/carts/${cartId}`, cartData);
+export const deleteCart = (cartId) => api.delete(`/carts/${cartId}`);
