717f61e3a47ed7c32aa4b878738c56c7c427cff4
Paul Kim <ppaauullkkiimm@gmail.com>
2025-01-15 15:53:29 +0100

    removed Cart duplicate and import in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Cart from './components/Cart';
-import MainLayout from './components/MainLayout';
-import { CartProvider } from './context/CartContext';
-import './App.css'
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import MainLayout from "./components/MainLayout";
+import { CartProvider } from "./context/CartContext";
+import "./App.css";
 
 function App() {
   return (
     <CartProvider>
-    <Router>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="cart" element={<Cart />} />
-        </Route>
-      </Routes>
-    </Router>
-  </CartProvider>
+      <Router>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="cart" element={<Cart />} />
+          </Route>
+        </Routes>
+      </Router>
+    </CartProvider>
   );
 }
 
