a4ce09e77f65018a5c4c4444290435c7749d9287
Paul Kim <ppaauullkkiimm@gmail.com>
2025-01-15 16:53:49 +0100

    CartTable and Cart added

---
+++ src/components/CartTable.jsx
@@ -0,0 +1,62 @@
+import { useCart } from "../context/CartContext";
+
+const CartTable = () => {
+  const { cart, removeFromCart } = useCart();
+
+  const totalAmount = cart.reduce(
+    (total, item) => total + item.price * item.quantitiy,
+    0
+  );
+
+  return (
+    <div className="overflow-x-auto">
+      <table className="table table-zebra w-full">
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Total</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cart.map((item) => (
+            <tr key={item.id}>
+              <td>
+                <img
+                  src={item.image}
+                  alt={item.title}
+                  className="w-16 h-16 object-cover"
+                />
+              </td>
+              <td>{item.title}</td>
+              <td>{item.quantity}</td>
+              <td>${item.price.toFixed(2)}</td>
+              <td>${(item.price * item.quantity).toFixed(2)}</td>
+              <td>
+                <button
+                  onClick={() => removeFromCart(item.id)}
+                  className="btn btn-md btn-error"
+                >
+                  Remove
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan="4" className="text-right font-bold">
+              Total:
+            </td>
+            <td className="font-bold">${totalAmount.toFixed(2)}</td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
+};
+
+export default CartTable;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,3 +1,32 @@
-function Cart() {}
+import { Link } from "react-router-dom";
+import { useCart } from "../context/CartContext";
+import CartTable from "../components/CartTable";
 
+function Cart() {
+  const { cart } = useCart();
+
+  if (cart.length === 0) {
+    return (
+      <div className="card bg-base-100 shadow-xl max-w-md mx-auto mt-10">
+        <div className="card-body items-center text-center">
+          <h2 className="card-title text-2xl">Your cart is empty!</h2>
+          <p className="text-base-content pl-1">
+            Add some products to make your cart happy.
+          </p>
+          <div className="card-actions justify-center">
+            <Link to="/" className="btn btn-primary btn-wide">
+              Let's go shopping!
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="container mx-auto p-4">
+      <h2 className="text-3xl font-bold mb-6 text-center">Your Cart</h2>
+      <CartTable />
+    </div>
+  );
+}
 export default Cart;
