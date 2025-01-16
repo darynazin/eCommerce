2d1a0ca4f351244a278f0df1d3ee606e6272df9a
Paul Kim <ppaauullkkiimm@gmail.com>
2025-01-16 10:47:34 +0100

    cart page and cart table

--- src/components/CartTable.jsx
+++ src/components/CartTable.jsx
@@ -1,85 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useCart } from "../context/CartContext";
 
 const CartTable = () => {
-  const { cart, removeFromCart, addToCart, decrementFromCart } = useCart();
+  const { cart, removeFromCart, addToCart, decrementFromCart, clearCart } =
+    useCart();
+  const [showCheckoutPopup, setShowCheckoutPopup] = useState(false);
 
   const totalAmount = cart.reduce(
     (total, item) => total + item.price * item.quantity,
     0
   );
 
+  const handleCheckout = () => {
+    setShowCheckoutPopup(true);
+  };
+
+  const handleClosePopup = () => {
+    setShowCheckoutPopup(false);
+    clearCart(); // Clear the cart when closing the popup
+  };
+
   return (
-    <div className="overflow-x-auto">
-      <table className="table table-zebra w-full">
-        <thead>
-          <tr>
-            <th>Image</th>
-            <th>Name</th>
-            <th>Quantity</th>
-            <th>Price</th>
-            <th>Total</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cart.map((item) => (
-            <tr key={item.id}>
-              <td>
-                <img
-                  src={item.image}
-                  alt={item.title}
-                  className="w-16 h-16 object-cover"
-                />
-              </td>
-              <td>{item.title}</td>
-              <td>
-                <button
-                  onClick={() => decrementFromCart(item.id)}
-                  className="bg-slate-950 text-white p-2 rounded-md hover:bg-slate-800"
-                >
-                  -
-                </button>
-                <span className="mr-2">{item.quantity}</span>
-                <button
-                  onClick={() => addToCart(item)}
-                  className="bg-slate-950 text-white p-2 rounded-md hover:bg-slate-800"
-                >
-                  +
-                </button>
-              </td>
-              <td>${item.price.toFixed(2)}</td>
-              <td>${(item.price * item.quantity).toFixed(2)}</td>
-              <td>
-                <button
-                  onClick={() => removeFromCart(item.id)}
-                  className="bg-red-500 text-white p-2 rounded-md hover:bg-red-400"
-                >
-                  Remove
-                </button>
-              </td>
+    <div className="flex flex-col h-full">
+      <div
+        className="overflow-x-auto flex-grow"
+        style={{ maxHeight: "calc(100vh - 200px)" }}
+      >
+        <table className="table table-zebra w-full">
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Quantity</th>
+              <th>Price</th>
+              <th>Total</th>
+              <th>Action</th>
             </tr>
-          ))}
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colSpan="4" className="text-right font-bold">
-              Total:
-            </td>
-            <td className="font-bold">${totalAmount.toFixed(2)}</td>
-            <td></td>
-          </tr>
-        </tfoot>
-      </table>
-      <div className="mt-4 text-right">
-        <Link
-          to="/"
-          className="bg-slate-950 text-white p-2 rounded-md hover:bg-slate-800"
-        >
+          </thead>
+          <tbody>
+            {cart.map((item) => (
+              <tr key={item.id}>
+                <td>
+                  <img
+                    src={item.image}
+                    alt={item.title}
+                    className="w-16 h-16 object-cover"
+                  />
+                </td>
+                <td>{item.title}</td>
+                <td>
+                  <button
+                    onClick={() => decrementFromCart(item)}
+                    className="bg-slate-950 text-white p-2 rounded-md hover:bg-slate-800"
+                  >
+                    -
+                  </button>
+                  <span className="m-4">{item.quantity}</span>
+                  <button
+                    onClick={() => addToCart(item)}
+                    className="bg-slate-950 text-white p-2 rounded-md hover:bg-slate-800"
+                  >
+                    +
+                  </button>
+                </td>
+                <td>${item.price.toFixed(2)}</td>
+                <td>${(item.price * item.quantity).toFixed(2)}</td>
+                <td>
+                  <button
+                    onClick={() => removeFromCart(item.id)}
+                    className="bg-red-500 text-white p-2 rounded-md hover:bg-red-400"
+                  >
+                    Remove
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="mt-4 flex justify-between items-center">
+        <Link to="/" className="btn btn-seconday text-black p-2 rounded-md">
           Continue Shopping
         </Link>
+        <div className="flex items-center">
+          <span className="text-xl font-bold mr-4">
+            Total: ${totalAmount.toFixed(2)}
+          </span>
+          <button
+            onClick={handleCheckout}
+            className="bg-slate-950 text-white p-2 rounded-md hover:bg-slate-800"
+          >
+            Checkout
+          </button>
+        </div>
       </div>
+      {showCheckoutPopup && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white p-4 rounded-md">
+            <p className="mb-4">Checkout was successful!</p>
+            <button
+              onClick={handleClosePopup}
+              className="bg-slate-950 text-white p-2 rounded-md hover:bg-slate-800 items-center justify-center"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -7,14 +7,14 @@ function Cart() {
 
   if (cart.length === 0) {
     return (
-      <div className="card bg-base-100 shadow-xl max-w-md mx-auto mt-10">
+      <div className="card bg-base-100 shadow-xl max-w-md mx-auto mt-20">
         <div className="card-body items-center text-center">
           <h2 className="card-title text-2xl">Your cart is empty!</h2>
           <p className="text-base-content pl-1">
             Add some products to make your cart happy.
           </p>
           <div className="card-actions justify-center">
-            <Link to="/" className="btn btn-primary btn-wide">
+            <Link to="/" className="btn btn-neutral btn-md">
               Let's go shopping!
             </Link>
           </div>
@@ -23,8 +23,10 @@ function Cart() {
     );
   }
   return (
-    <div className="container mx-auto p-4">
-      <h2 className="text-3xl font-bold mb-6 text-center">Your Cart</h2>
+    <div className="container mx-auto p-2">
+      <h3 className="text-xl font-bold mb-6 text-center">
+        Your Shopping Bag 🛍️
+      </h3>
       <CartTable />
     </div>
   );
